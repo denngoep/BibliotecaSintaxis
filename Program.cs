286aa0bddc81b6c066ff0136ee969a03d0808927
Dennis Gonzalez <degonzalez376@gmail.com>
2026-03-22 12:33:40 -0500

    feat: add books menu

--- Program.cs
+++ Program.cs
@@ -11,14 +11,29 @@ class Program
 
         Console.ReadLine();
     }
-    
-  static void ShowMainMenu()
-{
-    Console.WriteLine("1. Libros");
-    Console.WriteLine("2. Usuarios");
-    Console.WriteLine("3. Préstamos");
-    Console.WriteLine("4. Búsquedas y reportes");
-    Console.WriteLine("5. Guardar / Cargar datos");
-    Console.WriteLine("6. Salir");
-}
+
+    static void ShowMainMenu()
+    {
+        Console.WriteLine("1. Libros");
+        Console.WriteLine("2. Usuarios");
+        Console.WriteLine("3. Préstamos");
+        Console.WriteLine("4. Búsquedas y reportes");
+        Console.WriteLine("5. Guardar / Cargar datos");
+        Console.WriteLine("6. Salir");
+
+        string option = Console.ReadLine();
+
+        if (option == "1")
+        {
+            ShowBooksMenu();
+        }
+    }
+
+    static void ShowBooksMenu()
+    {
+        Console.WriteLine("=== MENÚ LIBROS ===");
+        Console.WriteLine("1. Agregar libro");
+        Console.WriteLine("2. Listar libros");
+        Console.WriteLine("3. Volver");
+    }
 }
