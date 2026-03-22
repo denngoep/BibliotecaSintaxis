4652f8c4341c1a4157b4515fb1c13c4a4918c4ba
Dennis Gonzalez <degonzalez376@gmail.com>
2026-03-22 12:03:07 -0500

    feat: add main menu options

--- Program.cs
+++ Program.cs
@@ -8,12 +8,17 @@ class Program
         Console.WriteLine("Bienvenido al sistema");
 
         ShowMainMenu();
-        
+
         Console.ReadLine();
     }
     
-    static void ShowMainMenu()
-    {
+  static void ShowMainMenu()
+{
     Console.WriteLine("1. Libros");
-    }
+    Console.WriteLine("2. Usuarios");
+    Console.WriteLine("3. Préstamos");
+    Console.WriteLine("4. Búsquedas y reportes");
+    Console.WriteLine("5. Guardar / Cargar datos");
+    Console.WriteLine("6. Salir");
+}
 }
